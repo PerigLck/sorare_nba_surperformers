03eaf4cab7fd7e47ec079aca249e0ba56b3ed6fa
Perig Cossec--kerloc'h <perig.cosseckerloch@roullier.com>
2023-01-27 11:15:16 +0100

    update

--- get_surperformer_players.py
+++ get_surperformer_players.py
@@ -6,7 +6,8 @@ if len(sys.argv) != 2:
     print("Usage : python3 get_surperformer_players.py surperform_percent")
     #quit()
 
-prct = int(sys.argv[1])/100 + 1
+# prct = int(sys.argv[1])/100 + 1
+prct = 1.35
 conf = get_config("/mnt/c/Users/perig.cosseckerloch/test/conf/config.json")
 
 l10_worksheet = xlrd.open_workbook(conf.get('L10_FILE_PATH')).sheet_by_index(0)
@@ -17,7 +18,10 @@ for i in range(2, l10_worksheet.nrows):
 previsions_l10_worksheet = xlrd.open_workbook(conf.get('PREVISION_FILE_PATH')).sheet_by_index(0)
 d_predicted = {}
 for i in range(1, previsions_l10_worksheet.nrows):
-    d_predicted[previsions_l10_worksheet.cell_value(i, 0).lower()]=previsions_l10_worksheet.cell_value(i, 22)
+    d_predicted[previsions_l10_worksheet.cell_value(i, 0).lower()]={
+        "score":previsions_l10_worksheet.cell_value(i, 22),
+        "match":'{}-{}'.format(previsions_l10_worksheet.cell_value(i, 1), previsions_l10_worksheet.cell_value(i, 2))
+    }
 
 d_final={}
 for key, value in d_l10.items():
@@ -29,10 +33,11 @@ for key, value in d_l10.items():
 
 surperformers={}
 for key, value in d_final.items():
-    if value.get("Predicted") and value.get("l10") and prct*value["l10"]<value["Predicted"] and value["l10"]>10:
+    if value.get("Predicted") and value.get("l10") and prct*value["l10"]<value["Predicted"].get("score") and value["l10"]>10:
         surperformers[key]={
             "l10":value.get("l10"),
-            "Predicted":d_predicted.get(key)
+            "Predicted": d_predicted.get(key).get('score'),
+            "Match": d_predicted.get(key).get('match')
         }
 
 print("Here are the surperformers players having a prediction {} percent above their l10 :\n{}".format(prct, surperformers))
